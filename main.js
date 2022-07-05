2278274e45f79329a8cd7b342722c25b67c74cd9
manvithsai <90837315+manvithsai@users.noreply.github.com>
2022-07-05 15:21:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,82 @@
-function startclassification(){
-    navigator.mediaDevices.getUserMedia({audio:true});
-    classifier=ml5.soundClassify('https://storage.googleapis.com/tm-model/u02fOYRBx/model.json',modelReady)
+canvas=document.getElementById('myCanvas');
+ctx=canvas.getContext("2d");
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
 }
-function modelReady(){
-    classifier.classify(gotResults);
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
-function gotResults(error,results){
-    if(error){
-    console.log(error);
-    }
-    else{
-    console.log(results);
-    random_number_r=Math.floor(Math.random()*255)+1;
-    random_number_g=Math.floor(Math.random()*255)+1;
-    random_number_b=Math.floor(Math.random()*255)+1;
-    document.getElementById("sound").innerHTML="I can hear- "+results[0].label;
-    document.getElementById("accurecy-of-sound").innerHTML="accurecy- "+(results[0].confidence*100).toFixed(2)+"%";
-    document.getElementById("sound").style.color="rgb("+random_number_r+","+ random_number_g+","+random_number_b+")";
-    document.getElementById("accurecy-of-sound").style.color="rgb("+random_number_r+","+ random_number_g+","+random_number_b+")";
-    img=document.getElementById("dog");
-    img-2=document.getElementById("cat");
-    img-3=document.getElementById("lion");
-    img-4=document.getElementById("bird");
-    if(results[0].label=="bark"){
-        img.src='dog.gif';
-        img-2.src='lion.jpg';
-        img-3.src='cat.jpg';
-        img-4.src='bird.jpg';
-    }
-    else if(results[0].label=="roar"){
-        img.src='dog.jpg';
-        img-2.src='lion.gif';
-        img-3.src='cat.jpg';
-        img-4.src='bird.jpg';
-    }
-    else if(results[0].label=="meow"){
-        img.src='dog.jpg';
-        img-2.src='lion.jpg';
-        img-3.src='cat.gif';
-        img-4.src='bird.jpg';
-    }
-    else{
-        img.src='dog.jpg';
-        img-2.src='lion.jpg';
-        img-3.src='cat.jpg';
-        img-4.src='bird.gif';  
-    }
-    }
-    }
+
+//Write a code to grab the key-pressed event
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{
+		aplhabetkey();
+		document.getElementById("d1").innerHTML="you pressed Alphabet key";
+		console.log("alphabetkey");
+		}
+else if(( keyPressed >=48 && keyPressed <=57))	{
+numberkey()
+document.getElementById("d1").innerHTML="you pressed Number key";
+console.log("numberkey");
+}
+else if(( keyPressed >=37 && keyPressed <=40||keyPressed==17||keyPressed==18||keyPressed==27)){
+	specialkey()
+	document.getElementById("d1").innerHTML="you pressed Special key";
+	console.log("specialkey");
+	}
+else if(( keyPressed >=37 && keyPressed <=40)){
+arrowkey()
+document.getElementById("d1").innerHTML="you pressed Arrow key";
+console.log("arrowkey");
+}
+}
+
+function aplhabetkey()
+{
+	img_image="Alpkey.png";
+	add();
+}
+
+
+function numberkey()
+{
+	img_image="numkey.png";
+	add();
+}	
+
+function arrowkey()
+{
+	img_image="Arrkey.png";
+	add();
+}
+
+function specialkey()
+{
+	img_image="spkey.png";
+	add();
+}	
+
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
